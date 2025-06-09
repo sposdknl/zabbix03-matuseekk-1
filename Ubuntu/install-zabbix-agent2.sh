beed558d16972be5aa988264703663d715113e15
matuseekk <125639655+matuseekk@users.noreply.github.com>
2025-06-09 15:23:49 +0200

    Add files via upload

--- Ubuntu/install-zabbix-agent2.sh
+++ Ubuntu/install-zabbix-agent2.sh
@@ -1,24 +1,22 @@
 #!/usr/bin/env bash
 
-# Instalace balicku net-tools
-sudo apt-get install -y net-tools
+# Instalace balíčku net-tools (volitelné)
+sudo apt-get install -y net-tools wget
 
-# Stažení balíčku pro instalaci zabbix repo
-sudo wget https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_latest+ubuntu22.04_all.deb
+# Stažení správného Zabbix 7.0 repozitáře
+wget https://repo.zabbix.com/zabbix/7.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_7.0-1+ubuntu22.04_all.deb
+sudo dpkg -i zabbix-release_7.0-1+ubuntu22.04_all.deb
 
-# Instalace meta balíčku
-sudo dpkg -i zabbix-release_latest+ubuntu22.04_all.deb
-
-# Aktualizace repository
+# Aktualizace repozitářů a instalace agenta
 sudo apt-get update
+sudo apt-get install -y zabbix-agent2
 
-# Instalace meta balíčku
-sudo apt-get install -y zabbix-agent2 zabbix-agent2-plugin-*
+# Konfigurace Zabbix agenta
+sudo sed -i 's/^Server=.*/Server=192.168.1.2/' /etc/zabbix/zabbix_agent2.conf
+sudo sed -i 's/^ServerActive=.*/ServerActive=192.168.1.2/' /etc/zabbix/zabbix_agent2.conf
+sudo sed -i 's/^Hostname=.*/Hostname=ubuntu-'$(date +%s)'/' /etc/zabbix/zabbix_agent2.conf
+echo "HostMetadata=SPOS" | sudo tee -a /etc/zabbix/zabbix_agent2.conf
 
-# Povoleni sluzby zabbix-agent2
+# Aktivace a spuštění služby
 sudo systemctl enable zabbix-agent2
-
-# Restart sluzby zabbix-agent2
 sudo systemctl restart zabbix-agent2
-
-# EOF
